12047dfb84852e934825a39803bbc6718dbb501b
Kushagra Srivastava <kushagras481@gmail.com>
2021-10-02 23:13:48 -0700

    first push

---
+++ src/Constants/URLEnums.js
@@ -0,0 +1,6 @@
+export const URL = {
+    BASE_URL: 'http://localhost:3000',
+    FORGOT_PASSWORD_EXT: '/forgotpassword',
+    SIGNUP_EXT: '/signup',
+    SETTINGS_EXT: '/settings'
+} 
---
+++ src/Popup/Components/Display/TitleBar.jsx
@@ -0,0 +1,19 @@
+import React from 'react'
+
+import '../../Styles/Components/TitleBarStyles.scss'
+
+import { URL } from '../../../Constants/URLEnums.js'
+
+function TitleBar(props){
+    const url = URL.BASE_URL
+
+    return (
+        <div id='title-bar'>
+            <h1 id='title-text'>
+                <a href={url} target="_blank" rel="noopener noreferrer">SupportYouYoutuber</a>
+            </h1>
+        </div>
+    )
+}
+
+export default TitleBar
